008e768702f5592ff33cf6eb9059de2536fdb4d1
Tim Clark <tclark@highwirepress.com>
2020-10-11 09:47:29 +0100

    added test.tf and .gitignore

---
+++ test.tf
@@ -0,0 +1,152 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+# Provider configuration
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+#==========================#
+# Networking configuration #
+#==========================#
+
+resource "aws_vpc" "tims-vpc" {
+ cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_default_security_group" "tims-default-security-group" {
+  vpc_id = aws_vpc.tims-vpc.id
+}
+
+resource "aws_subnet" "tims-subnet" {
+  vpc_id     = aws_vpc.tims-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-1c"
+}
+
+resource "aws_internet_gateway" "tims-igw" {
+    vpc_id = aws_vpc.tims-vpc.id
+}
+
+resource "aws_route_table" "tims-aws-route-table" {
+    vpc_id = aws_vpc.tims-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.tims-igw.id
+    }
+}
+
+resource "aws_route_table_association" "tims-route-table-association" {
+    subnet_id = aws_subnet.tims-subnet.id
+    route_table_id = aws_route_table.tims-aws-route-table.id
+}
+
+resource "aws_network_acl" "tims-nacl" {
+  vpc_id = aws_vpc.tims-vpc.id
+}
+
+resource "aws_network_acl_rule" "tims-nacl-rule" {
+  network_acl_id = aws_network_acl.tims-nacl.id
+  rule_number    = 200
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_security_group" "tims-sg-allow-ssh" {
+  name        = "tims-sg-allow-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.tims-vpc.id
+
+  ingress {
+    description = "SSH for VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#==========================#
+#    Auto scaling config   #
+#==========================#
+resource "aws_launch_configuration" "tims-launch-configuration" {
+  name_prefix   = "tims-launch-configuration-"
+  image_id      = "ami-0bfa4fefe067b7946"
+  instance_type = "t2.micro"
+  associate_public_ip_address = false
+  key_name      = "incomprehensible-keypair"
+  security_groups = [aws_security_group.tims-sg-allow-ssh.id]
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "tims-autoscaling-group" {
+  name                 = "tims-autoscaling-group"
+  launch_configuration = aws_launch_configuration.tims-launch-configuration.name
+  min_size             = 1
+  max_size             = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = true
+  vpc_zone_identifier       = [aws_subnet.tims-subnet.id]
+  load_balancers = [aws_elb.tims-elb.id]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#==========================#
+#   ELB configuration      #
+#==========================#
+
+resource "aws_elb" "tims-elb" {
+  name               		  = "tims-elb"
+  subnets            		  = [aws_subnet.tims-subnet.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+  security_groups = [aws_security_group.tims-sg-allow-ssh.id,aws_default_security_group.tims-default-security-group.id]
+
+  listener {
+    instance_port     = 22
+    instance_protocol = "tcp"
+    lb_port           = 22
+    lb_protocol       = "tcp"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 25
+    target              = "TCP:22"
+    interval            = 30
+  }
+}
+
+#===========================#
+#   Route 53 configuration	#
+#===========================#
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z326GWB4WZIEJJ"
+  name    = "terraform.incomprehensible.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_elb.tims-elb.dns_name]
+}
